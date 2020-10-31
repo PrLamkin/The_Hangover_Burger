2109fa19b035b23e961684776202af9f7705a2b5
PrLamkin <shiektheimmortal@outlook.com>
2020-10-30 19:48:32 -0500

    borgar

---
+++ models/burger.js
@@ -0,0 +1,30 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(callback) {
+        orm.selectAll("burger", function(res) {
+            callback(res);
+        });
+    },
+
+    insertOne: function(cheese, callback) {
+        orm.insertOne("burger", "burger_name", cheese, function(buns) {
+            console.log("burgerJS cheese: ", cheese, "buns: ", buns);
+            callback(buns);
+        });
+    },
+
+    updateOne: function(colVal, id, cb) {
+        orm.updateOne(colval, id, function(res) {
+            cb(res);
+        });
+    },
+
+    deleteOne: function(id, callback) {
+        orm.deleteOne(id, function(res) {
+            callback(res);
+        });
+    }
+};
+
+module.exports = burger;
