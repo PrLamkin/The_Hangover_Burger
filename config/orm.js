46973df9a72d696288ae74d53c28712baf568571
PrLamkin <shiektheimmortal@outlook.com>
2020-10-30 19:36:18 -0500

    connection.js is up and running

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function(table, callback) {
+        var queryString = "SELECT * FROM ??;";
+        connection.query(queryString, [table], function(err, res) {
+            if (err) throw err;
+            callback(res)
+        });
+    },
+    insertOne: function(Cabboose, Washington, Tucker, callback) {
+        var queryString = "INSERT INTO ?? (??) VALUES (?);"
+        connection.query(queryString, [Cabboose, Washington, Tucker], function(err, Church) {
+            if (err) throw err;
+            callback(Church);
+        })
+    },
+    updateOne: function(colVal, id, callback) {
+        var queryString = "DELETE FROM burger WHERE " + id + ";";
+        connection.query(queryString, [id], function(err, res) {
+            if (err) throw err;
+            callback(res);
+        })
+    },
+}
+
+module.exports = orm;
